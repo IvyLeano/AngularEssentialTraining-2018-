4469c6cca57795a9545fa1429e5d47fe1cbf422c
unknown <IvyLeano@outlook.com>
2019-09-03 14:28:39 -0400

    media-item.component.ts notes

--- app/media-item.component.ts
+++ app/media-item.component.ts
@@ -6,8 +6,9 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
   styleUrls: ['app/media-item.component.css']
 })
 export class MediaItemComponent {
-  @Input() mediaItem;
-  @Output() delete = new EventEmitter();
+  // for non native dom events - @input/@output
+  @Input() mediaItem; // when you use "mediaItem" in template, data bind mediaItem, if there is a parameter (an alias for mediaItem), data-bind when that alias is used
+  @Output() delete = new EventEmitter(); //exposing an even that can be subscribed to on a custom element
 
   onDelete() {
     this.delete.emit(this.mediaItem);
