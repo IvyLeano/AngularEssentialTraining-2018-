4d3bea035fc596b130732a398934449bd6fa5fad
unknown <IvyLeano@outlook.com>
2019-09-03 13:54:49 -0400

    mw-media-item

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,9 +1,10 @@
 // to be recognized as angular module, you need to use a decorator - '@'
-// to use the decorator, you need to import the package that exports it.
+// to use the decorator, you need to import the package that exports it - ngModule (for the root).
 // you can also import 2 modules, using a comma.
 import { ngModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser'; // contains pipes, core diectives and more for working with the d.o.m
 import { AppComponent } from './app.component';
+import { MediaItemComponent } from './media-item-component';
 
 // to use the decorator the below line is needed
 @ngModule(
@@ -15,7 +16,8 @@ import { AppComponent } from './app.component';
   ],
   declarations: [       // used to make components, directives and pipes available to your module, that don't come from another module
                         // any imports not from @angular go here 
-    AppComponent        // starting component
+    AppComponent,        // starting component
+    MediaItemComponent
   ],
   bootstrap: [          // used for a root module, and lets angular know which components will be the starting point for the bootstrap process, entry point for app code
      AppComponent       // root module listed here, it will be the starting point for bootstrap 
